c2cd5384f592af51defdd5b0f2dc5a65caf1900e
Achmad Fauzi <achmadfauzi@live.com>
2021-02-28 20:27:34 +0700

    first commit dart

---
+++ bin/dart_console_basic_oop.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_basic_oop/dart_console_basic_oop.dart' as dart_console_basic_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_basic_oop.calculate()}!');
+}
---
+++ test/dart_console_basic_oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_basic_oop/dart_console_basic_oop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
